60e625d901d49bd0774145cc21362daa1bce12b5
Andrew <brotzgea1@gmail.com>
2021-03-25 00:28:58 -0400

    added console menu

--- dinner.py
+++ dinner.py
@@ -1,21 +1,33 @@
 import random
-# from consolemenu import *
-# from consolemenu.items import *
 
-# menu = ConsoleMenu("DinDin", "It's what's for dinner!")
-
-# function_item = FunctionItem("See what's for dinner!", input, ["Enter an input"])
-
-# menu.append_item(function_item)
-
-# menu.show()
-
+show_menu = True
 approved = []
+filted_list = []
+filt_approved = approved.copy()
 
 class Restaurant:
     def __init__(self, name, cuisine):
         self.name = name
         self.cuisine = cuisine
+
+def menu():
+    print("Welcome to DinDin!")
+    print("1. See what's for dinner!")
+    print("2. Not in the mood for something? Let us know.")
+    print("3. Add a restaurant from the list.")
+    print("4. Delete a restaurant from the list.")
+    print("5. Exit")
+    selector = input("What would you like to do? Pick a number. ")
+    if selector == "1":
+        randomize()
+    elif selector == "2":
+        filter()
+    elif selector == "3":
+        addrest()
+    elif selector == "4":
+        delrest()
+    elif selector == "5":
+        raise SystemExit
 
 def addrest():
     name = input("What is the name of the restaurant? ")
@@ -25,17 +37,22 @@ def addrest():
     approved.append(first)
 
 def delrest():
-    value = input("What is the name of the restaurant? ")
+    rest_one = input("What is the name of the restaurant? ")
     for item in approved:
-        if value == item.name:
+        if rest_one == item.name:
             approved.remove(item)
 
-def randomize():
-    value = random.choice(approved)
+def filter():
+    filt_one = input("What aren't you in the mood for? ")
     for item in approved:
-        if item == value:
+        if item == filt_one:
+            filt_approved.remove(item)
+            
+def randomize():
+    rando = random.choice(filt_approved)
+    for item in filt_approved:
+        if item == rando:
             print(item.name)
 
-addrest()
-addrest()
-randomize()
+# while show_menu:
+#     menu()
